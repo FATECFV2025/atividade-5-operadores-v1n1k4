a3fc826ade70c2ea3eb2524915d265156760ac12
v1n1k4 <vini.castilho09@gmail.com>
2025-11-07 12:52:10 -0300

    Update App.java

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -1,29 +1,12 @@
-import java.util.Scanner;
-
 public class App {
-        //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in);
+    public static void main(String[] args) {
+        Operadores op = new Operadores();
 
-        //variável local 
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
-
+        System.out.println("O Resultado é: " + op.adicao(10, 5));
+        System.out.println("O Resultado é: " + op.subtracao(10, 5));
+        System.out.println("O Resultado é: " + op.multiplicacao(10, 5));
+        System.out.println("O Resultado é: " + op.divisao(10, 5));
 
     }
 }
