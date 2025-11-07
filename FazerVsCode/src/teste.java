f87d59c1f7f9590cfa0462a2160358ef32a816e2
v1n1k4 <vini.castilho09@gmail.com>
2025-11-07 12:52:46 -0300

    Update teste.java

--- FazerVsCode/src/teste.java
+++ FazerVsCode/src/teste.java
@@ -1,7 +1,38 @@
-public class teste {
+public class Operadores {
 
-    public int atributo1;
+    private float v1;
+    private float v2;
 
-    
-    
+    public float getV2() {
+        return v2;
+    }
+
+    public void setV2(float v2) {
+        this.v2 = v2;
+    }
+
+    public float getV1() {
+        return v1;
+    }
+
+    public void setV1(float v1) {
+        this.v1 = v1;
+    }
+
+    public float adicao(float v1, float v2) {
+        return v1 + v2;
+    }
+
+    public float subtracao(float v1, float v2) {
+        return v1 - v2;
+    }
+
+    public float multiplicacao(float v1, float v2) {
+        return v1 * v2;
+    }
+
+    public float divisao(float v1, float v2) {
+        return v1 / v2;
+    }
+
 }
