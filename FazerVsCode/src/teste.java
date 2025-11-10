75fe2c4d09a5580f26f229f647107109b73b12ea
v1n1k4 <vini.castilho09@gmail.com>
2025-11-10 17:22:40 -0300

    Update teste.java

--- FazerVsCode/src/teste.java
+++ FazerVsCode/src/teste.java
@@ -1,7 +1,9 @@
 public class Operadores {
 
+  
     private float v1;
     private float v2;
+
 
     public float getV2() {
         return v2;
@@ -32,7 +34,67 @@ public class Operadores {
     }
 
     public float divisao(float v1, float v2) {
+        //so pra fala q sabo mas se for float n precisa ne? eu acho,talvez eu n sabo tanto
+        if(v2 == 0){
+            return 0;
+        }
+
         return v1 / v2;
     }
+    // vou tenta fazer o desafio aq
+    //op logicos
+    public  boolean operadordosEx(boolean treco, boolean treco2){
+        return treco && treco2;
+    }
+    public boolean operadordosOr(boolean treco, boolean treco2){
+        return treco || treco2;
+    }
+    public boolean operadoresdeNot(boolean treco){
+        return !treco;
+    }
+    //tenta o dos comparativo
+    public boolean duploIgual(float v1, float v2){
+        return v1 == v2;//igualdade 
+
+    }
+    public boolean diferente(float v1, float v2){
+        return v1 != v2;
+
+    }
+    public boolean maio(float v1, float v2){
+        return v1 > v2;
+
+    }
+    public boolean meno(float v1, float v2){
+        return v1 < v2;
+
+    }
+    public boolean maioouIgual(float v1, float v2){
+        return v1 >= v2;
+
+    }
+    public boolean menoouIgual(float v1, float v2){
+        return v1 <= v2;
+
+    }
+    //atreibuição
+    public  float atribuicaoSoma(float v1, float v2){
+        return v1 += v2;
+
+    }
+    public float atribuicaoMeno(float v1, float v2){
+        return v1 -= v2;
+
+    }
+    public float atribuicaoMultipli(float v1, float v2){
+        return v1 *= v2;
+
+    }
+    public float atribuicaoDivida(float v1, float v2){
+        return v1 /= v2;
+    }
+
+    
+
 
 }
